b6eb977bc648f30ba21e06dbefb3d5056e63917c
Abukalam <101173635+Gladiator57@users.noreply.github.com>
2022-05-31 21:30:02 +0530

    Add files via upload
    Hi, 
    these were the main codes that I've practiced in java Programming.

---
+++ Selectionsort.java
@@ -0,0 +1,32 @@
+public class Selectionsort {  
+    public static void SSelection(int[] arr){  
+        for (int i = 0; i < arr.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+    }  
+       
+    public static void main(String a[]){  
+        int[] arr1 = {2,84,959,6,0,52};  
+        System.out.println("Before Selection Sort");  
+        for(int i:arr1){  
+            System.out.print(i+" ");  
+        }  
+        System.out.println();  
+          
+        SSelection(arr1);  
+         
+        System.out.println("After Selection Sort");  
+        for(int i:arr1){  
+            System.out.print(i+" ");  
+        }  
+    }  
+}  
